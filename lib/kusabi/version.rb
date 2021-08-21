15cbeed62dfd71a5d6e0de155292547883240b0c
ppmasa8 <kiyokiyopip56@gmail.com>
2021-08-21 21:47:45 +0900

    initial commit

---
+++ lib/kusabi/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Kusabi
+  VERSION = "0.1.0"
+end
---
+++ spec/kusabi_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Kusabi do
+  it "has a version number" do
+    expect(Kusabi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
